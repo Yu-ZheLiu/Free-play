3ded09d41047ff30a64a22107e411bdd95fa6e4f
Yu-ZheLiu <kevin155096@gmail.com>
2016-11-22 23:47:56 +0800

    2016-11-22 wc

--- wc/wordcloud.js
+++ wc/wordcloud.js
@@ -5,6 +5,7 @@ function FOODwordcloud(){
   d3.wordcloud()
   .size([800, 400])
   .selector('#wordcloud')
+  .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
   .words(data)
   .start();
   });
