d4aac4873119fba4fd7c947bb9baee2b49ff24dd
Yu-ZheLiu <kevin155096@gmail.com>
2016-12-25 17:02:54 +0800

    js

--- AsterPlot/TravelLight-master/AsterPlot/AsterPlot.js
+++ AsterPlot/TravelLight-master/AsterPlot/AsterPlot.js
@@ -52,6 +52,7 @@ d3.csv('https://yu-zheliu.github.io/Free-play/AsterPlot/TravelLight-master/Aster
       .attr("class", "solidArc")
       .attr("stroke", "gray")
       .attr("d", arc)
+      .attr("div",tip.show)
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
 
