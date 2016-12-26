528132b79dee824c88199024217602f0a253032e
Yu-ZheLiu <kevin155096@gmail.com>
2016-12-26 21:47:43 +0800

    zoom

--- AsterPlot/TravelLight-master/AsterPlot/AsterPlot.js
+++ AsterPlot/TravelLight-master/AsterPlot/AsterPlot.js
@@ -31,9 +31,9 @@ var outlineArc = d3.arc()
         .outerRadius(radius);
 
 var svg = d3.select("div").append("svg")
-    .attr("width", '100%')
+    .attr("width", width)
     .attr("class", "solidArc")
-    .attr("height", '100%')
+    .attr("height", height)
     .append("g")
     //.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
     .call(zoom);
